a3e715cb908a476ebdb33b432d11638e79ba06ed
dipesh_patel <dipesh_patel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-05 11:37:12 +0000

    tagging 3.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-widget-sugarcrm-lead-module/tags/3.0@1060303 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin-functions.php
@@ -0,0 +1,428 @@
+<?php
+if (realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
+    exit('Please don\'t access this file directly.');
+}
+
+add_action('init', 'do_output_buffer');
+function do_output_buffer() {
+        ob_start();
+}
+
+### Plugin Update database changes Logic START
+function OEPL_plugin_update_function() {
+    global $OEPL_update_version,$wpdb;
+	$OEPL_current_version = get_option("OEPL_PLUGIN_VERSION");
+    if ($OEPL_current_version != $OEPL_update_version) {
+    	$sql = 'SHOW COLUMNS FROM '.OEPL_TBL_MAP_FIELDS;
+		$rows = $wpdb->get_col($sql);
+		if(!in_array('wp_custom_label',$rows)){
+			$wpdb->query('ALTER TABLE '.OEPL_TBL_MAP_FIELDS.' ADD `wp_custom_label` VARCHAR( 50 ) NOT NULL AFTER `wp_meta_label`');
+		}
+		if(!in_array('required',$rows)){
+			$wpdb->query("ALTER TABLE ".OEPL_TBL_MAP_FIELDS." ADD `required` ENUM( 'Y', 'N' ) NOT NULL DEFAULT 'N' AFTER `display_order`");
+		}
+		if(!in_array('hidden',$rows)){
+			$wpdb->query("ALTER TABLE ".OEPL_TBL_MAP_FIELDS." ADD `hidden` ENUM('Y','N') NOT NULL DEFAULT 'N' AFTER `required`;");
+		}
+		if(!in_array('custom_field',$rows)){
+			$wpdb->query("ALTER TABLE ".OEPL_TBL_MAP_FIELDS." ADD `custom_field` ENUM( 'Y', 'N' ) NOT NULL DEFAULT 'N';");
+		}
+		update_option('OEPL_PLUGIN_VERSION',$OEPL_update_version);
+    }
+}
+add_action('plugins_loaded', 'OEPL_plugin_update_function' );
+### Plugin Update database changes Logic END
+
+### Change Wordpress Default Upload dir START
+function OEPL_Change_Upload_Dir($upload) {
+	$upload['subdir']	= OEPL_FILE_UPLOAD_FOLDER;
+	$upload['path']		= $upload['basedir'] . $upload['subdir'];
+	$upload['url']		= $upload['baseurl'] . $upload['subdir'];
+	return $upload;
+}
+### Change Wordpress Default Upload dir END
+
+## Front end Save Function
+add_action('wp_ajax_WidgetForm', 'WidgetForm');
+add_action('wp_ajax_nopriv_WidgetForm', 'WidgetForm');
+function WidgetForm(){
+	global $objSugar, $wpdb, $_SESSION, $_POST, $_GET;
+	$successMsg = get_option('OEPL_SugarCRMSuccessMessage');
+	$failureMsg = get_option('OEPL_SugarCRMFailureMessvvage');
+	$EmailNotification	= get_option('OEPL_Email_Notification');
+	if($_POST['captcha'] != ($_SESSION['captcha1'] + $_SESSION['captcha2']) ){	
+		echo "Invalid captcha code.Please try again";
+	} else {
+		if ( ! function_exists( 'wp_handle_upload' ) ) require_once( ABSPATH . 'wp-admin/includes/file.php' );
+		if(count($_FILES) > 0){
+			$FileArray = array();
+			add_filter('upload_dir', 'OEPL_Change_Upload_Dir');
+			foreach($_FILES as $key=>$file){
+				$upload_overrides = array( 'test_form' => false );
+				$movefile = wp_handle_upload( $file , $upload_overrides );
+				if ( $movefile ) {
+					if($movefile['error']){
+						echo "Sorry ! '".$file['name']."' colud not be uploaded due to security reasons.<br>";
+					}else{
+						$movefile['name'] = $file['name'];
+						$FileArray[] = $movefile;
+					}
+				} else {
+				    echo "Error occured while uploading file. Please try again<br>";
+				}
+			}
+			remove_filter('upload_dir', 'OEPL_Change_Upload_Dir');
+		}
+		$a = $objSugar->InsertLeadToSugar($FileArray);
+		if($a != false) {
+			echo $successMsg;
+			if($EmailNotification && $EmailNotification == "Y"){
+				$emailTo = get_option('OEPL_Email_Notification_Receiver');
+				$query = "SELECT * FROM ".OEPL_TBL_MAP_FIELDS." WHERE is_show = 'Y' ORDER BY custom_field DESC,display_order";
+				$RS = $wpdb->get_results($query,ARRAY_A);
+				$message = '<h3>Lead Description</h3><table border="0">';
+				$is_attach_avail = 'N';
+				foreach($RS as $attr){
+					if($attr['custom_field'] != 'Y')
+					{
+						$message .= '<tr><th>';
+						$message .= $attr['wp_meta_label']." : </th><td>".$_POST[$attr['wp_meta_key']]."<br />";
+						$message .= '</td></tr>';
+					} else {
+						$is_attach_avail = 'Y';
+					}
+				}
+				if($is_attach_avail == 'Y'){
+					$message .= '<tr><th>';
+					$message .= "Attachments</th><td> : All attachments are available in your SugarCRM history/notes module";
+					$message .= '</td></tr>';
+				}
+				$message .= "</table>";
+				$Subject = "Lead generated from ".get_bloginfo('name')."";
+				add_filter( 'wp_mail_content_type','OEPL_set_mail_contenttype' );
+				wp_mail( $emailTo, $Subject, $message );
+				remove_filter( 'wp_mail_content_type','OEPL_set_mail_contenttype' );
+			}
+		} else {
+			echo $failureMsg;
+		}
+	}
+	$random1 = rand(1,9);
+	$random2 = rand(1,9);
+	$_SESSION['captcha1'] = $random1;
+	$_SESSION['captcha2'] = $random2;
+	die();
+}
+## Front end Save Function End
+
+function OEPL_set_mail_contenttype(){
+    return "text/html";
+}
+
+## Save sugarCRM config START
+add_action('wp_ajax_OEPLsaveConfig', 'OEPLsaveConfig');
+function OEPLsaveConfig(){
+	$TestConn = new OEPLSugarCRMClass;
+	$TestConn->SugarURL  = $_POST['SugarURL'];
+	$TestConn->SugarUser = $_POST['SugarUser']; 
+	$TestConn->SugarPass = md5($_POST['SugarPass']); 
+	$t = $TestConn->LoginToSugar();
+	if(strlen($t)>10)
+	{
+		update_option('OEPL_SUGARCRM_URL' , $_POST['SugarURL']);
+		update_option('OEPL_SUGARCRM_ADMIN_USER' , $_POST['SugarUser']);
+		update_option('OEPL_SUGARCRM_ADMIN_PASS' , md5($_POST['SugarPass']));
+		echo 'Configuration saved successfully';
+	} else {
+		echo "Invalid login details. Please try again";
+	}
+	die();
+}
+## Save sugarCRM config END
+
+##Lead fileds Sync function START
+add_action('wp_ajax_LeadFieldSync', 'LeadFieldSync');
+function LeadFieldSync(){
+	global $objSugar;
+	$t = $objSugar->LoginToSugar();
+	if(!strlen($t)>10)
+	{
+		echo "Fields synchronization failed. Please try again";
+		die();	
+	} else {
+		FieldSynchronize();
+		echo "Fields synchronized successfully.";
+		die();
+	}
+}
+##Lead fileds Sync function END
+
+##General message save function START
+add_action('wp_ajax_SugarGeneralMsg', 'SugarGeneralMsg');
+function SugarGeneralMsg()
+{
+	$successMsg = get_option('OEPL_SugarCRMSuccessMessage');
+	$failureMsg = get_option('OEPL_SugarCRMFailureMessage');
+	if(!empty($_POST))
+	{
+		update_option("OEPL_SugarCRMSuccessMessage",$_POST['SuccessMessage']);
+		update_option("OEPL_SugarCRMFailureMessage",$_POST['FailureMessage']); 
+		update_option("OEPL_auto_IP_addr_status",$_POST['IPaddrStatus']);
+		update_option("OEPL_Email_Notification",$_POST['EmailNotification']);
+		update_option("OEPL_Email_Notification_Receiver",$_POST['EmailReceiver']);
+		echo "General settings saved successfully.";
+		die();
+	}
+}
+##General message save function END
+
+## Save Custom Browse field START
+add_action("wp_ajax_OEPL_Custom_Field_Save","OEPL_Custom_Field_Save");
+function OEPL_Custom_Field_Save(){
+	global $wpdb;
+	$FieldName = $_POST['Field_Name'];
+	$Filtered_FieldName = str_replace(' ','_',$_POST['Field_Name']);
+	
+	$query = "SELECT * FROM ".OEPL_TBL_MAP_FIELDS." WHERE wp_meta_label = '".$FieldName."'";
+	$RS = $wpdb->get_results($query);
+	
+	if(count($RS) > 0){
+		echo "Duplicate field already exist. Please try with a different field name.";
+	} else {
+		$insArray = array ('module'			=>'OEPL',
+						   'field_type'		=>'file',
+						   'data_type'		=>'file',
+
+						   'wp_meta_label' 	=> $FieldName,
+						   'is_show'		=> 'Y',
+						   'custom_field' 	=> 'Y'
+						  );
+		$insert = $wpdb->insert (OEPL_TBL_MAP_FIELDS , $insArray);
+		if ($insert != FALSE) {
+			$where = array('pid' => $wpdb->insert_id);
+			$updArray = array ('field_name' 	=> 'oepl_browse_'.$wpdb->insert_id,
+							   'wp_meta_key' 	=> 'oepl_browse_'.$wpdb->insert_id);
+			$update = $wpdb->update( OEPL_TBL_MAP_FIELDS ,$updArray,$where);
+			if ($update != FALSE){
+				echo "Field added successfully";
+			} else {
+				echo "Error occured. Please try again";
+			}
+		} else {
+			echo 'Problem adding field. Please try again';
+		}
+	}
+	die();
+}
+## Save Custom Browse field END
+
+## DELETE custom browse field START
+add_action("wp_ajax_OEPL_Custom_Field_Delete","OEPL_Custom_Field_Delete");
+function OEPL_Custom_Field_Delete(){
+	global $wpdb;
+	$pid = $_POST['pid'];
+	$where = array ( 'pid' => $pid );
+	$delete = $wpdb->delete( OEPL_TBL_MAP_FIELDS , $where);
+	if($delete != FALSE){
+		echo "Field deleted successfully";	
+	} else {
+		echo "Error occured ! Please try again";
+	}
+	die();
+}
+## DELETE custom browse field END
+
+##Submenu under SugarCRM Menu START
+function OEPL_SugarCRM_Submenu_function(){
+	echo "<div class='wrap'>";
+	echo "<h1>SugarCRM Lead module field list</h1>";
+	echo '<table class="OEPL_add_field_box">
+		  	<tr height="25">
+		  		<td><img src="'.OEPL_PLUGIN_URL.'image/plus-icon.png" valign="center" class="OEPL_hide_panel" is_show="No" /></td>
+		  		<td colspan="4" style="font-size:15px" valign="center">Add Custom Browse Field</td>
+		  	</tr>
+		  	<tr class="OEPL_hidden_panel" style="display:none">
+		  		<td></td>
+		  		<td width="80">Field name : </td>
+		  		<td width="80"><input type="text" id="OEPL_Custom_Field_Name" name="OEPL_Custom_Field_Name" /></td>
+		  		<td align="left"><button class="button button-primary OEPL_Custom_Field_Add">Add Field</button></td>
+		  	</tr>
+		  	<tr class="OEPL_hidden_panel" style="display:none">
+		  		<td></td>
+		  		<td colspan="4"><span class="description"><strong>Note:</strong> Uploaded files will be available in Notes module of SugarCRM and History subpanel of Lead module.</span></td>
+		  	</tr>
+		  </table>';
+	echo '<form id="OEPL-Leads_table" method="post">';
+	require_once(OEPL_PLUGIN_DIR . 'Fields_map_table.php');
+	$table = new Fields_Map_Table;
+	echo '<input type="hidden" name="page" value="mapping_table" />';
+	$table->search_box('Search', 'LeadSearchID');
+	$table->prepare_items();
+	$table->display();
+	echo '</form>';
+	echo "</div>";
+}
+##Submenu under SugarCRM Menu END
+
+## SugarCRM Password Encryption
+/***************************************
+ * Code commented because sometimes hook
+ * does not fire because some conflicts 
+ * in tyhemes and other plugins
+ * *************************************
+add_action('update_option','SetHaleSugarCRMFields', 10, 2);
+function SetHaleSugarCRMFields(){
+	global $objSugar, $wpdb;
+	if(!empty($_POST))
+	{
+		if($_POST['OEPL_SUGARCRM_ADMIN_PASS'] != '')
+		{
+			delete_option( 'OEPL_SUGARCRM_ADMIN_PASS' );
+			add_option( 'OEPL_SUGARCRM_ADMIN_PASS', md5($_POST['OEPL_SUGARCRM_ADMIN_PASS']), NULL, 'no' );
+		}
+				
+		$objSugar = new OEPLSugarCRMClass;
+		$objSugar->SugarURL  = trim($_POST['OEPL_SUGARCRM_URL']);
+		$objSugar->SugarUser = trim($_POST['OEPL_SUGARCRM_ADMIN_USER']); 
+		$objSugar->SugarPass = md5($_POST['OEPL_SUGARCRM_ADMIN_PASS']);
+	
+		$t = $objSugar->LoginToSugar();
+		if(!strlen($t)>10)
+		{
+			return false;	
+		}
+		FieldSynchronize();
+		return null;
+	}
+} */
+## SugarCRM Password Encryption
+
+function FieldSynchronize()
+{
+	global $objSugar, $wpdb;
+
+	if($objSugar->SugerSessID == '')
+	{
+		$t = $objSugar->LoginToSugar();
+	}
+	if(!strlen($objSugar->SugerSessID)>10)
+	{
+		return false;	
+	}
+	
+	## Start - Set Module Fields in Table
+	foreach($objSugar->ModuleList as $key => $val)
+	{
+		$ModuleName = $val;
+		$ModuleFileds = $objSugar->getLeadFieldsList();
+		$SugarFlds = array();
+		if(count($ModuleFileds->module_fields) > 0)
+		{
+		foreach($ModuleFileds->module_fields as $fkey => $fval)
+		{
+			$fType = $fval->type;
+			$insAry = array();
+			switch($fType)
+			{
+				case 'enum':
+					$insAry['field_type']  = 'select';
+					$insAry['field_value'] = serialize($fval->options);
+					break;
+				case 'radioenum':
+					$insAry['field_type']  = 'radio';
+					$insAry['field_value'] = serialize($fval->options);
+					break;
+				case 'bool':
+					$insAry['field_type']  = 'checkbox';
+					$insAry['field_value'] = serialize($fval->options);
+					break;
+				case 'text':
+					$insAry['field_type'] 	= 'textarea';
+					$insAry['field_value'] 	= '';
+					break;
+				case 'file':
+					$insAry['field_type'] 	= 'file';
+					$insAry['field_value'] 	= '';
+					break;
+				default:
+					$insAry['field_type']  = 'text';
+					$insAry['field_value'] = '';
+					break;
+			}
+			$insAry['module'] 		 = $ModuleName;
+			$insAry['field_name'] 	 = $fkey;
+			$insAry['wp_meta_key'] 	 = OEPL_METAKEY_EXT . strtolower($ModuleName). '_' . $fkey;
+			$insAry['wp_meta_label'] = $fval->label;
+			$insAry['data_type'] 	 = $fval->type;
+			$insAry['wp_meta_label'] = str_replace(':','', trim($insAry['wp_meta_label']) );
+				
+			$query = "SELECT count(*) as tot FROM ".OEPL_TBL_MAP_FIELDS." 
+					  WHERE module = '".$insAry['module'] . "' AND field_name = '".$insAry['field_name'] . "'";
+			$RecCount = $wpdb->get_results($query, ARRAY_A);
+			
+			if(!in_array($insAry['field_name'], $objSugar->ExcludeFields) )
+			{
+				$SugarFlds[] = $insAry['field_name'];
+				if( $RecCount[0]['tot'] <= 0 )
+				{
+					$sql = "INSERT INTO ".OEPL_TBL_MAP_FIELDS." SET 
+							module 		  = '".$insAry['module'] . "' , 
+							field_type 	  = '".$insAry['field_type'] . "' , 
+							data_type 	  = '".$insAry['data_type'] . "' , 
+							field_name 	  = '".$insAry['field_name'] . "' , 
+							field_value   = '".$insAry['field_value'] . "' , 
+							wp_meta_label = '".$insAry['wp_meta_label'] . "' , 
+							wp_meta_key   = '".$insAry['wp_meta_key'] . "' ";
+					$wpdb->query($sql);
+				} else {
+					$sql = "UPDATE ".OEPL_TBL_MAP_FIELDS." SET 
+								module 		  = '".$insAry['module'] . "' , 
+								field_type 	  = '".$insAry['field_type'] . "' , 
+								data_type 	  = '".$insAry['data_type'] . "' , 
+								field_name 	  = '".$insAry['field_name'] . "' , 
+								field_value   = '".$insAry['field_value'] . "' , 
+								wp_meta_label = '".$insAry['wp_meta_label'] . "' , 
+								wp_meta_key   = '".$insAry['wp_meta_key'] . "' 
+						   WHERE module = '".$insAry['module'] . "' AND field_name = '".$insAry['field_name'] . "'";
+					$wpdb->query($sql);
+				}
+			}
+		}
+		}
+		$query   = "SELECT pid, field_name, wp_meta_key FROM ".OEPL_TBL_MAP_FIELDS." 
+					WHERE module = '".$insAry['module'] . "'";
+		$WPFieldsRS = $wpdb->get_results($query, ARRAY_A);
+		$fcnt = count($WPFieldsRS);
+		$WPFields = array();
+		for($i=0; $i<$fcnt; $i++)
+		{
+			if(!in_array($WPFieldsRS[$i]['field_name'], $SugarFlds) )
+			{
+				$delSql = "DELETE FROM ".OEPL_TBL_MAP_FIELDS." WHERE pid = ".$WPFieldsRS[$i]['pid']." AND module = '".$ModuleName."'"; 
+				$wpdb->query($delSql);
+			}
+		}
+	}
+	## End - Set Module Fields in Table
+}
+
+## start AJAX functions
+add_action('wp_ajax_TestSugarConn', 'TestSugarConn');
+function TestSugarConn()
+{	
+	$TestConn = new OEPLSugarCRMClass;
+	$TestConn->SugarURL  = $_POST['URL'];
+	$TestConn->SugarUser = $_POST['USER']; 
+	$TestConn->SugarPass = md5($_POST['PASS']); 
+	$t = $TestConn->LoginToSugar();
+
+	if(strlen($t)>10)
+	{
+		echo "Connection successfully established.";
+	} else {
+		echo "Connection failed. Please check detail.";
+	}
+	die();
+}
+## end   AJAX functions
+?>
---
+++ readme.txt
@@ -0,0 +1,129 @@
+=== WordPress to SugarCRM Lead ===
+Contributors: dipesh_patel
+Support : http://www.offshoreevolution.com/
+Donate link: 
+Tags: SugarCRM, Sugar CRM, web-to-lead SugarCRM, web-to-lead Sugar CRM, contact form Sugarcrm, contact us sugarCRM, web to lead sugarCRM, SugarCRM customization, SugarCRM customer portal, SugarCRM WordPress integration, collect info and send to Sugar CRM
+Minimum Requirement : 3.4
+Requires at least: 3.4
+Tested up to: 4.1
+Stable tag: 3.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+WordPress to SugarCRM Lead Plug-in submit your custom form data to your SugarCRM Lead module. This will provided Widget to Connect your SugarCRM.
+
+== Description ==
+
+**No license fees, 100% FREE, No Ads, No BloatWares**
+
+**Follow us on [Facebook](https://www.facebook.com/WordPressFreePluginSugarCRMLead) , [Google+](https://plus.google.com/106573739543605091204) & [Youtube](https://www.youtube.com/playlist?list=PLnVyVy-trUeBhkdzwwp0_mO16QuZhxx1k).**
+
+WordPress to SugarCRM Lead plugin is simple yet advance, easy & one time setup solution for your business needs. Plugin will dinamically generate a Lead-form on a Widget based on your choices using fields which are mapped to your SugarCRM Lead module. You can change the Order & Label of the field any time you want. **Now convert your website traffic/visitors into business Leads**. It was never that easy before.
+
+[youtube https://www.youtube.com/watch?v=pGTgSAbVY-s]
+
+**Features**
+
+* 2 minute setup
+* No HTML
+* User Friendly Form builder
+* Dynamically generated
+* Easy to manage
+* Pass any field as hidden field with any value you want. You can also change that value anytime you want
+* Mark any field mandatory
+* Change success/failure message
+* Capture user/visitor's Remote Address
+* Option to attach unlimited files with every lead.
+* Get Email notifications for each lead generation.
+
+**Upcoming features**
+
+* Create multiple forms using SugarCRM Lead fields.
+* Select Form in Widget.
+* Generate Shot-Code to set form anywhere in your WordPress website.
+
+**Setup**
+
+1. Download & Enable plugin
+2. Test connection with your SugarCRM & save your SugarCRM credentials
+3. Synchronize Lead Fields
+4. Now all your Lead fields will be mapped in 'Lead module' table. Choose which fields you want to display on widget
+5. Go to widget page Drag & Drop SugarCRM Lead Form Widget to wherever you want (P.S Only one widget per page)
+
+*That's it ! As simple as that !*
+
+**For SugarCRM Settings**
+
+SugarCRM URL : Set your REST API SugarCRM URL. For customized SugarCRM, REST API URL would be “http OR https://&lt;Your Domain Name&gt;/service/v4_1/rest.php”. Service URL may be depend on your SugarCRM version.
+
+[Click here](http://support.sugarcrm.com/02_Documentation/04_Sugar_Developer/Sugar_Developer_Guide_7.2/70_API/Web_Services/00_API_Versioning/) to check your SugarCRM version and get REST API.
+
+**Important !**
+
+* You will require SugarCRM administrator level credentials to Plug-in it
+* You can use Widget one time only on each page, if will be display 2 time at single page – it will not work
+* You must save your SugarCRM URL, Username & Password without which plugin won't work
+* To use 'Pass user Remote Address with every lead' function you must create custom field in your SugarCRM lead module named 'lead_remote_ip'
+* Now make any field hidden & provide it's value in Widget arguments in Widgets page. You can optimize this feature for something like from which part of your website lead is generated
+
+== Installation ==
+
+1. Download & Enable plugin.
+2. Test connection with your SugarCRM & save your SugarCRM credentials.
+3. Synchronize Lead Fields.
+4. Now all your Lead fields will be mapped in 'Lead module' table. Choose which fields you want to display on widget.
+5. Go to widget page Drag & Drop SugarCRM Lead Form Widget to wherever you want.(P.S Only one widget per page)
+
+== Frequently Asked Questions ==
+
+1. Which user name I need to set? 
+
+Answer: You must have to set Administrator level user/pass to submit form data into SugarCRM Lead module. Once you have successfully tested your credentials, Click on SAVE button to save your User/Password into WordPress database.
+
+2.Leads not generating on SugarCRM side ?
+
+Answer : You haven't saved your SugarCRM credentials properly. Go to your SugarCRM menu check your login details once again by 'Test connection' and then click 'Save changes'. It should work now provided if details are correct.
+
+3. Is my credentials safe on WordPress?
+
+Answer: Yes, Your credentials will be stored in MD5 format. So, nobody can access or read your credentials even from database.
+
+== Screenshots ==
+
+1. Plugin settings page.
+2. List of Lead module fields.
+3. Widget page with a hidden field.
+4. Form with calendar on webpage.
+5. Form on webpage generated dynamically.
+
+== Release ==
+3.0
+
+== Upgrade Notice ==
+Not Applicable
+
+== Change log ==
+<strong>Version 3.0</strong><br />
+Release Date : 5th January 2015
+
+1. Option to attach unlimited files with every lead.
+2. Get Email notifications for each lead generation.
+3. Design changes.
+
+<strong>Version 2.1.6</strong><br />
+Release Date : 17th December 2014
+
+1. Changed the method of saving SugarCRM settings to prevent conflicts.
+
+<strong>Version 2.0</strong><br />
+Release Date : 9th December 2014
+
+1. Now you can capture user's Remote Address with every lead.
+2. Option to pass any field as hidden field. You can set value for that hidden field in Widget arguments. With this feature you can get information about from which part of your website lead is generated.
+
+<strong>Version 1.5</strong><br />
+Release Date : 27th November 2014
+
+1. Now you can change the Label for any field.
+2. Option to mark any field mandatory.
+3. Provided calander plugin for date & datetime fields.
