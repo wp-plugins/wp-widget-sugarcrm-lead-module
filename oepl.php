95c9b742fdd0f99b983d9206f4bf0fb6bc168936
dipesh_patel <dipesh_patel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-16 05:14:31 +0000

    commit 3.1 !
    git-svn-id: https://plugins.svn.wordpress.org/wp-widget-sugarcrm-lead-module/tags/3.1@1069071 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ oepl.php
@@ -0,0 +1,46 @@
+<?php
+ /*
+ Plugin Name: WordPress to SugarCRM Lead
+ Plugin URI: http://www.offshoreevolution.com/
+ Description: This plugin will provided Widget to Connect your SugarCRM Lead module.
+ Version: 3.1
+ Author: Offshore Evolution Pvt Ltd
+ Author URI: http://www.offshoreevolution.com/
+ License: GPL
+ */
+
+require_once ("oepl.conf.php");
+
+/* Runs when plugin is activated */
+register_activation_hook(__FILE__, 'WPOEPLInstall');
+
+/* Runs on plugin deactivation*/
+register_deactivation_hook(__FILE__, 'WPOEPLUninstall');
+
+function WPOEPLInstall() {
+	$ins = new OEPLSugarCRMClass;
+	$ins -> Install();
+}
+
+function WPOEPLUninstall() {
+	$ins = new OEPLSugarCRMClass;
+	$ins -> UnInstall();
+}
+
+if (is_admin()) {
+	add_action('admin_menu', 'CreateMenu');
+}
+
+#### Session setting start
+add_action('init', 'OEStartSession', 1);
+add_action('wp_logout', 'OEEndSession');
+function OEStartSession() {
+    if(!session_id()) {
+        session_start();
+    }
+}
+function OEEndSession() {
+    session_destroy();
+}
+#### Session setting end
+?>
